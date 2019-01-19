a0aabf534721da82fb5ced9b5c110b576409b0e4
xyouwen <xyouwen@foxmail.com>
2019-01-20 01:40:41 +0800

    添加注释

--- API_AutoTest/src/main/java/pojo/CookieData.java
+++ API_AutoTest/src/main/java/pojo/CookieData.java
@@ -3,17 +3,28 @@ package pojo;
 /**
  * CookieData类
  * <p>cookie对象的三个属性：
- * <p>1、domainName: cookie对应的域名，即cookie是请求哪个地 
+ * <p>1、domainName: cookie对应的域名，即cookie是请求哪个地址产生的
  * <p>2、cookieKey：cookie的key；
  *<p> 3、cookieValue：cookie的value
  * 
  */
 public class CookieData {
-	
-	private String domainName; 
+
+	/**
+	 * 域名：cookie是由访问地址留下来的
+	 */
+	private String domainName;
+
+	/**
+	 * cookie的键
+	 */
 	private String cookieKey;
+
+	/**
+	 * cookie的值
+	 */
 	private String cookieValue;
-	
+
 	public String getDomaiName() {
 		return domainName;
 	}
